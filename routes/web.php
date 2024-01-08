d97df50994926b87572dce8c52c6a7650dca2d19
Fg120 <anwaria1404@gmail.com>
2024-01-08 14:30:46 +0700

    Install Template
    changing:
    routes web, home controller, blade templates, public assets

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
+// Route::get('/', function () {
+//     return view('home.index');
+// });
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/rooms', [HomeController::class, 'rooms'])->name('home.rooms');
+Route::get('/restaurant', [HomeController::class, 'restaurant'])->name('home.restaurant');
+Route::get('/about', [HomeController::class, 'about'])->name('home.about');
+Route::get('/blog', [HomeController::class, 'blog'])->name('home.blog');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
+Route::get('/rooms_single', [HomeController::class, 'rooms_single'])->name('home.rooms_single');
+Route::get('/blog_single', [HomeController::class, 'blog_single'])->name('home.blog_single');
